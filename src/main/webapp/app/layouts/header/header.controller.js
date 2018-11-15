1523cb3b901bdcae41a03b89285b08ab35354f47
Nifiro <nifiro@outlook.com>
2018-11-15 17:12:05 +0300

    Add book search support

--- src/main/webapp/app/layouts/header/header.controller.js
+++ src/main/webapp/app/layouts/header/header.controller.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('eLibraryApp')
         .controller('HeaderController', HeaderController);
 
-    HeaderController.$inject = ['$state', 'Auth', 'Principal', 'ProfileService', 'LoginService'];
+    HeaderController.$inject = ['$scope', '$http', '$state', 'Auth', 'Principal', 'ProfileService', 'LoginService'];
 
-    function HeaderController ($state, Auth, Principal, ProfileService, LoginService) {
+    function HeaderController($scope, $http, $state, Auth, Principal, ProfileService, LoginService) {
         var vm = this;
 
         vm.isNavbarCollapsed = true;
         vm.isAuthenticated = Principal.isAuthenticated;
 
-        ProfileService.getProfileInfo().then(function(response) {
+        ProfileService.getProfileInfo().then(function (response) {
             vm.inProduction = response.inProduction;
             vm.swaggerEnabled = response.swaggerEnabled;
         });
@@ -23,6 +23,43 @@
         vm.toggleNavbar = toggleNavbar;
         vm.collapseNavbar = collapseNavbar;
         vm.$state = $state;
+        vm.books = null;
+        vm.booksRecieved = false;
+        vm.foundBooks = [];
+
+        function getAllBooks() {
+            vm.foundBooks = [];
+            $http({
+                method: 'GET',
+                url: '/api/books?approved.equals=true'
+            }).success(function (data) {
+                filter(data);
+            });
+            console.log(vm.foundBooks);
+        }
+
+        function search() {
+            getAllBooks();
+        }
+
+        function filter(data) {
+            vm.books = data;
+            vm.booksRecieved = true;
+            vm.books.forEach(function (book) {
+                var title = book.title.toLowerCase();
+                var authorFirstName = book.authorFirstName.toLowerCase();
+                var authorLastName = book.authorLastName.toLowerCase();
+                var s = $scope.searchText.toLowerCase();
+                if (title.indexOf(s) !== -1
+                    || authorFirstName.indexOf(s) !== -1
+                    || authorLastName.indexOf(s) !== -1)
+                    vm.foundBooks.push(book);
+            });
+        }
+
+        $scope.toggled = function () {
+            search();
+        };
 
         function login() {
             collapseNavbar();
@@ -43,4 +80,5 @@
             vm.isNavbarCollapsed = true;
         }
     }
-})();
+})
+();
