894152d4f282ba3bad2f88df9051a270b9b4ff63
Amal <amal.muslim@yandex.ru>
2018-11-19 15:08:27 +0300

    admin widow profiles

--- src/main/webapp/app/entities/profile/profile.controller.js
+++ src/main/webapp/app/entities/profile/profile.controller.js
@@ -5,21 +5,30 @@
         .module('eLibraryApp')
         .controller('ProfileController', ProfileController);
 
-    ProfileController.$inject = ['Profile'];
+    ProfileController.$inject = ['Profile','Book'];
 
-    function ProfileController(Profile) {
+    function ProfileController(Profile,Book) {
 
         var vm = this;
 
         vm.profiles = [];
+        vm.books = [];
+
+
+    
 
         loadAll();
 
+        
         function loadAll() {
             Profile.query(function(result) {
                 vm.profiles = result;
+                console.log(vm.profiles);
                 vm.searchQuery = null;
             });
+            vm.books = Book.query();
+
+
         }
     }
 })();
