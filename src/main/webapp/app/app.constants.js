280dc5278f04f75106e85b5bb8c886dced870025
Amal <muslim.amal@yandex.ru>
2018-11-23 18:24:07 +0300

    ref

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('eLibraryApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1542980150294)
+        .constant('BUILD_TIMESTAMP', 1542985817612)
 ;
 })();
