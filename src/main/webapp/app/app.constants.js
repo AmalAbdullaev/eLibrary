299f5e502b7a19e03301c10c24641d37f03d0430
Amal <muslim.amal@yandex.ru>
2018-11-23 17:51:45 +0300

    refactor

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('eLibraryApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1542923390260)
+        .constant('BUILD_TIMESTAMP', 1542980150294)
 ;
 })();
