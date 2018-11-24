9b2fb9b566a34fff7b4a2b331f0a306d7aec0253
Nifiro <nifiro@outlook.com>
2018-11-24 23:11:04 +0300

    Implement infinite scroll on the "Favorite books" page

--- src/main/webapp/app/entities/favorite-book/favorite-book.controller.js
+++ src/main/webapp/app/entities/favorite-book/favorite-book.controller.js
@@ -1,40 +1,45 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('eLibraryApp')
         .controller('FavoriteBookController', FavoriteBookController);
 
-    FavoriteBookController.$inject = ['FavoriteBook','$state','Profile','Principal'];
+    FavoriteBookController.$inject = ['FavoriteBook', '$state', 'Profile', 'Principal', 'currentProfile'];
 
-    function FavoriteBookController(FavoriteBook,$state,Profile,Principal) {
+    function FavoriteBookController(FavoriteBook, $state, Profile, Principal, currentProfile) {
 
         var vm = this;
-
+        vm.page = 0;
+        vm.busy = false;
+        vm.itemsPerPage = 20;
+        vm.loadAll = loadAll;
+        vm.maxPage = Number.POSITIVE_INFINITY;
         vm.favoriteBooks = [];
+        vm.profile = currentProfile;
+        console.log(vm.profile.id);
 
-        loadAll();
-
-        vm.bookDelete = function(bookId){
-            FavoriteBook.delete({id:bookId},success);
-            function success(){
-                $state.reload();
-            }
-            
-        }
+        vm.bookDelete = function (index, bookId) {
+            vm.favoriteBooks.splice(index, 1);
+            FavoriteBook.delete({id: bookId});
+        };
 
         function loadAll() {
+            if (vm.busy) return;
+            vm.busy = true;
 
-            Principal.identity().then(function (user) {
-                Profile.getProfile({userId: user.id},onSuccess);
-                    function onSuccess(result){
-                        vm.profile = result;
-                        FavoriteBook.query({'profileId.equals': vm.profile.id},function(result) {
-                            vm.favoriteBooks = result;
-                            vm.searchQuery = null;
-                        });
-                    }                
-            })
+            FavoriteBook.query({
+                page: vm.page,
+                size: vm.itemsPerPage,
+                'profileId.equals': vm.profile.id
+            }, function (data, headers) {
+                vm.totalItems = headers('X-Total-Count');
+                vm.maxPage = vm.totalItems / vm.itemsPerPage;
+                for (var i = 0; i < data.length; i++)
+                    vm.favoriteBooks.push(data[i]);
+                vm.page++;
+                vm.busy = false;
+            });
         }
     }
 })();
