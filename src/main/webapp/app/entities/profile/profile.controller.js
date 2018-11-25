10314030ab69837f710c069340ab55c2de6e1e92
Nifiro <nifiro@outlook.com>
2018-11-25 11:16:26 +0300

    Implement infinite scroll on the "Profiles" page

--- src/main/webapp/app/entities/profile/profile.controller.js
+++ src/main/webapp/app/entities/profile/profile.controller.js
@@ -5,9 +5,9 @@
         .module('eLibraryApp')
         .controller('ProfileController', ProfileController);
 
-    ProfileController.$inject = ['$state', '$scope', '$http', 'Book', 'Profile'];
+    ProfileController.$inject = ['$state', '$scope', '$resource', '$http', 'Book', 'Profile'];
 
-    function ProfileController($state, $scope, $http, Book, Profile) {
+    function ProfileController($state, $scope, $resource, $http, Book, Profile) {
 
         var vm = this;
 
@@ -16,36 +16,60 @@
         vm.searchText = undefined;
         vm.busy = false;
         vm.page = 0;
+        vm.pageInSearch = 0;
         vm.itemsPerPage = 20;
         vm.maxPage = Number.POSITIVE_INFINITY;
-        vm.keyPress = keyPress;
-        vm.search = search;
+        vm.maxPageInSearch = Number.POSITIVE_INFINITY;
         vm.loadAll = loadAll;
+        vm.onChange = onChange;
 
-        function keyPress(event) {
-            if (event.keyCode === 13)
-                search();
-            else if (event.keyCode === 8 && vm.searchText.length === 0)
-                search();
+        Book.query({'approved.equals': true}, function (books, headers) {
+            vm.bookCount = headers('X-Total-Count');
+        });
+
+        function onChange() {
+            vm.profiles = [];
+            vm.pageInSearch = 0;
+            vm.page = 0;
+            loadAll();
         }
 
         function search() {
-            $http.get('/api/users/search?text=' + vm.searchText).success(function (profiles) {
-                vm.profiles = profiles;
+            $resource('/api/users/search', {}).query({
+                text: vm.searchText,
+                page: vm.pageInSearch,
+                size: vm.itemsPerPage
+            }, function (profiles, headers) {
+                vm.totalItemsInSearch = headers('X-Total-Count');
+                vm.maxPageInSearch = vm.totalItemsInSearch / vm.itemsPerPage;
+                for (var i = 0; i < profiles.length; i++)
+                    vm.profiles.push(profiles[i]);
+                vm.pageInSearch++;
+                vm.busy = false;
             });
         }
 
         function loadAll() {
             if (vm.busy) return;
             vm.busy = true;
-            Profile.query(function (data, headers) {
-                vm.totalItems = headers('X-Total-Count');
-                vm.maxPage = vm.totalItems / vm.itemsPerPage;
-                for (var i = 0; i < data.length; i++)
-                    vm.profiles.push(data[i]);
-                vm.page++;
-                vm.busy = false;
-            });
+            if (vm.searchText) {
+                search();
+            }
+            else {
+                Profile.query({
+                        page: vm.page,
+                        size: vm.itemsPerPage
+                    },
+                    function (data, headers) {
+                        vm.maxPageInSearch = Number.POSITIVE_INFINITY;
+                        vm.totalItems = headers('X-Total-Count');
+                        vm.maxPage = vm.totalItems / vm.itemsPerPage;
+                        for (var i = 0; i < data.length; i++)
+                            vm.profiles.push(data[i]);
+                        vm.page++;
+                        vm.busy = false;
+                    });
+            }
         }
     }
 })();
