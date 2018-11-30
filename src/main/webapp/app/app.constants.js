133a0f414be6061dd9d252e8276d56ea07549edc
Amal <muslim.amal@yandex.ru>
2018-11-30 16:42:55 +0300

    small fix

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,6 +5,6 @@
         .module('eLibraryApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', false)
-        .constant('BUILD_TIMESTAMP', 1543578334612)
+        .constant('BUILD_TIMESTAMP', 1543584429299)
 ;
 })();
